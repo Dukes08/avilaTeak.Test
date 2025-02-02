694794bdb41d46166787e992b9f6014a06b80888
Hugo Duque <duquehugo08@gmail.com>
2025-02-01 21:17:49 -0400

    fix(missing-api-photos): fix the error when the movie does not have an image associated

--- lib/data/mappers/movie_mapper.dart
+++ lib/data/mappers/movie_mapper.dart
@@ -10,14 +10,14 @@ class MovieMapper {
       adult: moviedb.adult,
       backdropPath: (moviedb.backdropPath != '')
           ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
-          : '',
+          : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=',
       genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
       id: moviedb.id,
       originalLanguage: moviedb.originalLanguage,
       originalTitle: moviedb.originalTitle,
       overview: moviedb.overview,
       popularity: moviedb.popularity,
-      posterPath: (moviedb.backdropPath != '')
+      posterPath: (moviedb.backdropPath != '' && moviedb.backdropPath != null)
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
           // este no-poster a la hora de adquierer los datos va a permitir filtrar con el where las peliculas que no tenga poster
           : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=',
@@ -28,16 +28,16 @@ class MovieMapper {
 
   static Movie movieDetailsToEntity(MovieDetails movie) => Movie(
       adult: movie.adult,
-      backdropPath: (movie.backdropPath != '')
+      backdropPath: (movie.backdropPath != '' && movie.backdropPath != null)
           ? 'https://image.tmdb.org/t/p/w500${movie.backdropPath}'
-          : '',
+          : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=',
       genreIds: movie.genres.map((e) => e.name).toList(),
       id: movie.id,
       originalLanguage: movie.originalLanguage,
       originalTitle: movie.originalTitle,
       overview: movie.overview,
       popularity: movie.popularity,
-      posterPath: (movie.backdropPath != '')
+      posterPath: (movie.backdropPath != '' && movie.backdropPath != null)
           ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
           // este no-poster a la hora de adquierer los datos va a permitir filtrar con el where las peliculas que no tenga poster
           : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=',
@@ -48,7 +48,7 @@ class MovieMapper {
 
   static Movie movieByActorToEntity(Cast movie) => Movie(
       adult: movie.adult,
-      backdropPath: (movie.backdropPath != '')
+      backdropPath: (movie.backdropPath != '' && movie.backdropPath != null)
           ? 'https://image.tmdb.org/t/p/w500${movie.backdropPath}'
           : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=',
       genreIds: movie.genreIds.map((e) => '$e').toList(),
@@ -57,10 +57,10 @@ class MovieMapper {
       originalTitle: '',
       overview: movie.overview,
       popularity: movie.popularity,
-      posterPath: (movie.backdropPath != '')
+      posterPath: (movie.backdropPath != '' && movie.backdropPath != null)
           ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
           // este no-poster a la hora de adquierer los datos va a permitir filtrar con el where las peliculas que no tenga poster
-          : 'no-poster',
+          : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=',
       title: movie.title,
       video: movie.video,
       voteAverage: movie.voteAverage,
